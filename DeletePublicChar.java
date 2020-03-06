fe7eeae0387974d244fc130d3ad1ffcf21a5f955
lisiyu06 <18392609095@163.com>
2020-03-06 22:23:44 +0800

    删除公共字符

---
+++ DeletePublicChar.java
@@ -0,0 +1,21 @@
+package day_04;
+
+import java.util.Scanner;
+
+/**
+ * Author: lisiyu
+ * Created: 2020/3/5
+ */
+
+// 删除公共字符
+
+public class DeletePublicChar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String str = scanner.nextLine();
+            String del = scanner.nextLine();
+
+        }
+    }
+}
