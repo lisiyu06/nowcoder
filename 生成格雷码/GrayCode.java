dcc1b28a61daecd5fb683b15938fb6e95ceac2d4
lisiyu06 <18392609095@163.com>
2020-03-27 21:38:34 +0800

    生成格雷码

---
+++ 生成格雷码/GrayCode.java
@@ -0,0 +1,25 @@
+package day_12;
+
+/**
+ * Author: lisiyu
+ * Created: 2020/3/27
+ */
+
+// 生成格雷码
+
+public class GrayCode {
+    public String[] getGray(int n) {
+        String[] resStrs = null;
+        if(n == 1) {
+            resStrs = new String[]{"0","1"};
+        } else {
+            String[] strs = getGray(n-1);
+            resStrs = new String[2*strs.length];
+            for(int i=0; i<strs.length; i++) {
+                resStrs[i] = "0"+strs[i];
+                resStrs[resStrs.length-1-i] = "1"+strs[i];
+            }
+        }
+        return resStrs;
+    }
+}
