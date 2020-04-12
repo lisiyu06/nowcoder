9e4bf96bd8b0521ed9b8755d5417344f9d8b999e
lisiyu06 <18392609095@163.com>
2020-04-12 21:15:41 +0800

    统计同成绩学生人数

---
+++ 统计同成绩学生人数/Main.java
@@ -0,0 +1,33 @@
+package day_16;
+
+import java.util.Scanner;
+
+/**
+ * Author: lisiyu
+ * Created: 2020/4/12
+ */
+
+// 统计同成绩学生人数
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        while (scan.hasNext()) {
+            int N = scan.nextInt();// 输入的人数
+            if (N == 0) {  // // 如果检测到输入的人数为0 则结束
+                return;
+            }
+            int[] arr = new int[N]; // 保存N个人的分数
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = scan.nextInt();
+            }
+            int t = scan.nextInt();// 目标分数 输出击中目标分数的个数
+            int count = 0;
+            for (int i = 0; i < arr.length; i++) {
+                if (t == arr[i]) {
+                    count++;
+                }
+            }
+            System.out.println(count);
+        }
+    }
+}
